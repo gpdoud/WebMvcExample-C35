d62e879aa0dfc3dd737c82257d6b3e4b259f5189
Greg Doud <gdoud@gmail.com>
2022-02-25 11:20:36 -0500

    Add project files.

---
+++ WebMvcExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebMvcExample.Models;
+
+namespace WebMvcExample.Controllers {
+
+    public class HomeController : Controller {
+
+        public IActionResult Index() {
+            ViewBag.Name = "Mary";
+            ViewBag.FV = 99999.99;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(FutureValueModel model) {
+            ViewBag.FV = model.CalculateFutureValue();
+            return View(model);
+        }
+    }
+}
---
+++ WebMvcExample/Models/FutureValueModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WebMvcExample.Models {
+
+    public class FutureValueModel {
+
+        public decimal MonthlyInvestment { get; set; }
+        public decimal YearlyInterestRate { get; set; }
+        public int Years { get; set; }
+
+        public decimal CalculateFutureValue() {
+            var months = Years * 12;
+            var monthlyInterestRate = YearlyInterestRate / 12 / 100;
+            var futureValue = 0m;
+            for(var i = 0; i < months; i++) {
+                futureValue = (futureValue + MonthlyInvestment) * (1 + monthlyInterestRate);
+            }
+            return futureValue;
+        }
+    }
+}
